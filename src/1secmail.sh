8d38a58939b15b48dd602277812e6ad958c8bcb2
huok <77536370+vraestoren@users.noreply.github.com>
2026-03-16 14:04:53 +0300

    Update 1secmail.sh

--- src/1secmail.sh
+++ src/1secmail.sh
@@ -3,34 +3,26 @@
 api="https://www.1secmail.com/api/v1"
 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 
+function _get() {
+    curl --request GET \
+        --url "$api/?$1" \
+        --user-agent "$user_agent" \
+        --header "accept: application/json" \
+        --header "content-type: application/json"
+}
+
 function generate_email() {
-	curl --request GET \
-		--url "$api/?action=genRandomMailbox&count=$1" \
-		--user-agent "$user_agent" \
-		--header "accept: application/json"	\
-		--header "content-type: application/json"
+    _get "action=genRandomMailbox&count=$1"
 }
 
 function get_domains() {
-	curl --request GET \
-		--url "$api/?action=getDomainList" \
-		--user-agent "$user_agent" \
-		--header "accept: application/json"	\
-		--header "content-type: application/json"
+    _get "action=getDomainList"
 }
 
 function get_messages() {
-	curl --request GET \
-		--url "$api/?action=getMessages&login=$1&domain=$2" \
-		--user-agent "$user_agent" \
-		--header "accept: application/json"	\
-		--header "content-type: application/json"
+    _get "action=getMessages&login=$1&domain=$2"
 }
 
 function read_message() {
-	curl --request GET \
-		--url "$api/?action=readMessage&login=$1&domain=$2&id=$3" \
-		--user-agent "$user_agent" \
-		--header "accept: application/json"	\
-		--header "content-type: application/json"
+    _get "action=readMessage&login=$1&domain=$2&id=$3"
 }
